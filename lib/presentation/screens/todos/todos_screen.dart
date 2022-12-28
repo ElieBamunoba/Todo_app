17fdf8d3f6a10b67b4691123c5dc5b1b7fe1c8b6
ElieBamunoba <eliebamunoba@gmail.com>
2022-12-28 18:47:06 +0300

    first commit

---
+++ lib/presentation/screens/todos/todos_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '10',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
